75c8de831deb1ed6de8dbe1b59b26db5ddbf2a64
Emmanuel Lechat <manu.lechat@free.fr>
2018-04-17 16:10:24 +0200

    page 404 & update Css

---
+++ error.php
@@ -0,0 +1,23 @@
+<?php include("header.php"); ?>
+<script>
+document.querySelector("#menu_desktop .a1").classList.add('active');
+</script>
+
+
+<!-- page vision -->
+
+<main class="page_error ">
+
+  <section class="layout_container center">
+
+
+    <img src="img/404.png" alt="" class='error_404'>
+
+  </section>
+
+
+</main>
+
+
+
+<?php  include("footer.php"); ?>
